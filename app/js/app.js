2d8f47788a7df12b59af8354f5f376d2deb2ea8b
Abhishek Malik <abhishek@credibase.com>
2014-02-02 02:51:57 +0530

    Basic layout of the site set. Getting external URL html is proving difficult.

--- app/js/app.js
+++ app/js/app.js
@@ -4,13 +4,46 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp', [
   'ngRoute',
+  'ngResource',
   'myApp.filters',
   'myApp.services',
   'myApp.directives',
   'myApp.controllers'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+])
+.config(function($routeProvider, $httpProvider) {
+  $httpProvider.defaults.useXDomain = true;
+  delete $httpProvider.defaults.headers.common['X-Requested-With'];
+  $httpProvider.defaults.transformResponse = [];
+  $httpProvider.defaults.transformRequest.push(function (data, headerGetter) {
+        console.log("transform Request");
+        return data;
+    });
+  $httpProvider.defaults.transformResponse = [];
+  $httpProvider.defaults.transformResponse.push(function (data, headerGetter) {
+        console.log("transform Response");
+        console.log(data);
+        return data;
+    });
+  $routeProvider.when('/', {templateUrl: 'partials/home.html', controller: 'Home'});
+  $routeProvider.otherwise({redirectTo: '/'});
+  $httpProvider.defaults.useXDomain = true;
+  delete $httpProvider.defaults.headers.common['X-Requested-With'];
+})
+// .config(['$routeProvider', ,'$httpProvider', function($routeProvider, $httpProvider) {
+//     // $httpProvider.defaults.useXDomain = true;
+//     // delete $httpProvider.defaults.headers.common['X-Requested-With'];
+//     $routeProvider.when('/', {templateUrl: 'partials/home.html', controller: 'Home'});
+//     $routeProvider.otherwise({redirectTo: '/'});
+//   }
+// ]);
+// .factory('XDomainData', function ($http) {
+//         return{
+//             doCrossDomainGet: function(url_address) {
+//                 return $http({
+//                     url:url_address,
+//                     method: 'GET'
+//                 })
+//             }
+//         }
+// })
+// ;
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,50 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+  controller('Home', function($scope, $http, $resource) {
+  // controller('Home', function($scope, $http, XDomainData) {
+    $scope.actor_names = '';
+    $scope.movies = [];
+    $scope.q_url1 = '';
+    // $scope.factory = XDomainData;
 
-  }])
-  .controller('MyCtrl2', [function() {
-
-  }]);
+    $scope.fetchResults = function(){
+        var movie_list = [
+            {title:'Oceans 11', year: 2000},
+            {title:'Oceans 12', year: 2004},
+            {title:'Oceans 13', year: 2006}
+        ];
+        console.log('Yay Search got clicked for:' + $scope.actor_names );
+        console.log($scope);
+        $scope.movies = movie_list;
+        var actor1 = $scope.actor_names.split(',')[0].trim().split(' ').join('+');
+        var actor2 = $scope.actor_names.split(',')[1].trim().split(' ').join('+');
+        $scope.q_url1 = 'http://www.imdb.com/find?q='+actor1+'&s=nm';
+        $scope.q_url2 = 'http://www.imdb.com/find?q='+actor2+'&s=nm';
+        // $scope.from_url1 = $scope.factory.doCrossDomainGet($scope.q_url1);
+        // $scope.from_url2 = $scope.factory.doCrossDomainGet($scope.q_url2);
+        // $http.get($scope.q_url1).success(function(data) {
+        //     console.log(data);
+        //     $scope.actor1_movies = data;
+        // });
+        $.ajax({
+          // url: "http://localhost:3311/get-data",
+          url: $scope.q_url1,
+          type: 'GET',
+          dataType: 'json'
+        })
+        .done(function(data) {
+            console.log(data);
+            // $rootScope.$apply(function() {d.resolve(data); });
+        })
+        .fail(function(data) {
+            // $rootScope.$apply(function() {d.reject(data); });
+        });
+        // var data1 = $resource($scope.q_url1);
+        // data1.get({}, function (data1) {
+        //     console.log(data1);
+        //     $scope.data1 = data1;
+        // });
+    }
+  })
+  ;
